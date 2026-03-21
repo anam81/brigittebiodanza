632438be05cda7575310409a9719adc8800c1110
anam81 <121813494+anam81@users.noreply.github.com>
2026-03-21 23:16:21 +0100

    Add files via upload

---
+++ videos_client.js
@@ -0,0 +1,106 @@
+const videos = [
+    {
+        id: "1174945206",
+        title: "ARD one Idents",
+        description: "I created these vibrant TV Channel Idents for ARD ONE."
+    },
+    {
+        id: "1175130607",
+        title: "Atmen: Breathe Your Life",
+        description: "I made the graphics for the ARD documentation 'Atmen: Breathe Your Life.' "+
+                    "Among others I composed and animated this energetic intro/opener.",
+        ratio: "235/100"
+    },
+    {
+        id: "1175321911",
+        title: "HydroDeSal",
+        description: "I created this animation for HydroDeSal, a research project focused on seawater desalination in the Persian Gulf. "+
+                    "The initiative explores decentralized systems that could provide reliable access to clean drinking water " +
+                    "for communities in the region.<br>"+
+                    "Directed by <a href='https://www.heinzenziob.de' target='_blank'>Florian Heinzen-Ziob</a><br>"+
+                    "<a href='https://www.youtube.com/watch?v=d9KdfnNRlV4' target='_blank'>Watch the full video</a>"
+    },
+    {
+        id: "260226326",
+        title: "energy future",
+        description: "This was for a pitch of an energy providers website relaunch. My job was to animate the pages.",
+        ratio: "16/10"
+    },
+    {
+        id: "221156939",
+        title: "Spotify Hashtag-Generator",
+        description: "I animated this video teasing Spotify´s Hashtag-Generator. Design by " +
+            "<a href='https://www.dayy.com/' target='_blank'>dayy</a>"
+    },
+    {
+        id: "1174525738",
+        title: "Opener for TV Show (pitch)",
+        description: "A pitch we did in 2015. We designed the On-Air Packaging for a German TV Channel Show (ZDF). "+
+                    "This is the opener.<br>"+
+                    "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>"
+    },
+    { 
+        id: "221156673", 
+        title: "Starkregen", 
+        description: "Graphic animation for science TV program 'Quarks & Co', WDR"
+    },
+    {
+        id: "221081352",
+        title: "Opener ZDF Adieu",
+        description: "I animated this opener for the tv show ZDF Adieu.<br>" +
+            "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>" +
+            "music: The Fall by Peter Rudenko licensed under CC BY 4.0"
+    },
+    {
+        id: "1175435060",
+        title: "ZDF Auslandsjournal",
+        description: "This graphic I designed and developed for ZDF Auslandsjournal: Die Doku: Kampf ums Wasser<br>" +
+                    "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>"
+    },
+    {
+        id: "1175443814",
+        title: "WDR Cosmo, Iran im Herzen",
+        description: "For a WDR Cosmo podcast trailer (Iran im Herzen), I animated these graphics as an intro.<br>" +
+            "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>"
+    },
+    { 
+        id: "222078676", 
+        title: "westart studio background", 
+        description: "I delevoped many of the screenbackgrounds of the WDR Westart live studio. These are some of those."
+    },
+    { 
+        id: "763383592", 
+        title: "Gesundes Sperma", 
+        description: "Compositing and animation for science tv program Quarks, WDR"
+    },
+    { 
+        id: "760875033", 
+        title: "Warum werden Tiere im Zoo getötet", 
+        description: "Animation for science tv program Quarks, WDR"
+    },
+    { 
+        id: "760873301", 
+        title: "7Dinge_Stimme", 
+        description: "Animation for science tv program Quarks, WDR"
+    },
+    { 
+        id: "260259258", 
+        title: "multiresistente Keime", 
+        description: "Animation for science tv program Quarks, WDR"
+    },
+    { 
+        id: "90454734", 
+        title: "ARD Radio Feature", 
+        description: 'I animated this TV Trailer for "Das ARD Radio Feature"'
+    },
+    {
+        id: "50453842",
+        title: "Counter IAA 2011",
+        description: "I designed and animated this counter for a car presentation at the IAA 2011."
+    },
+    { 
+        id: "74857016", 
+        title: "CityLeaks Documentary Titles", 
+        description: "Titles/Graphics for City Leaks Cologne Festival Documentary by Ömer Mutlu"
+    }
+]
